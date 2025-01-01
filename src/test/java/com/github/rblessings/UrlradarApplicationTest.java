31d5e1c194d54eb7604dfac084d3fd76b633261f
Ranzy Blessings <blessingsihembi@gmail.com>
2025-01-01 06:12:38 +0200

    fix: ensure tests run with dev profile enabled

--- src/test/java/com/github/rblessings/UrlradarApplicationTest.java
+++ src/test/java/com/github/rblessings/UrlradarApplicationTest.java
@@ -2,7 +2,9 @@ package com.github.rblessings;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles({"dev"})
 @SpringBootTest
 class UrlradarApplicationTest {
 
